11de68194d717f0f053ee547b5c9bdd3ef3abd24
Sandeep Somavarapu <sasomava@microsoft.com>
2024-10-07 11:56:15 +0200

    - implement chat participant - implement getFontSzie tool

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,96 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	// Create a chat participant
+	const chatParticipant = vscode.chat.createChatParticipant('c', async (request: vscode.ChatRequest, context: vscode.ChatContext, response: vscode.ChatResponseStream, token: vscode.CancellationToken) => {
+		const [model] = await vscode.lm.selectChatModels({
+			family: 'gpt-4o',
+		});
+
+		const tools = vscode.lm.tools
+			.filter(t => t.id === 'getFontSize')
+			.map<vscode.LanguageModelChatTool>(t => ({
+				name: t.id,
+				description: t.description,
+			}));
+
+		const messages = [
+			// vscode.LanguageModelChatMessage.User('You are a VS Code assistant and you are here to help user configuring VS Code.'),
+			vscode.LanguageModelChatMessage.User(request.prompt)
+		];
+
+		await invokeModelWithTools(messages, model, tools, request, response, token);
+
+	});
+
+	context.subscriptions.push(vscode.lm.registerTool('getFontSize', {
+		async invoke(options: vscode.LanguageModelToolInvocationOptions<void>, token: vscode.CancellationToken) {
+			return {
+				'text/plain': `${vscode.workspace.getConfiguration('editor').get('fontSize')}px`,
+			};
+		},
+	}));
+
+	context.subscriptions.push(chatParticipant);
+}
+
+interface IToolCall {
+	tool: vscode.LanguageModelToolDescription;
+	call: vscode.LanguageModelChatResponseToolCallPart;
+	result: Thenable<vscode.LanguageModelToolResult>;
+}
+
+async function invokeModelWithTools(initialMessages: vscode.LanguageModelChatMessage[], model: vscode.LanguageModelChat, tools: vscode.LanguageModelChatTool[], request: vscode.ChatRequest, response: vscode.ChatResponseStream, token: vscode.CancellationToken) {
+
+	const messages = [...initialMessages];
+	const toolCalls: IToolCall[] = [];
+
+	const modelResponse = await model.sendRequest(messages, { tools });
+
+	for await (const message of modelResponse.stream) {
+
+		if (message instanceof vscode.LanguageModelChatResponseTextPart) {
+			response.markdown(message.value);
+		}
+
+		else if (message instanceof vscode.LanguageModelChatResponseToolCallPart) {
+			const tool = vscode.lm.tools.find(t => t.id === message.name);
+			if (!tool) {
+				continue;
+			}
+			// TODO support prompt-tsx here
+			const requestedContentType = 'text/plain';
+			toolCalls.push({
+				call: message,
+				result: vscode.lm.invokeTool(tool.id, {
+					toolInvocationToken: request.toolInvocationToken,
+					requestedContentTypes: ['text/plain'],
+					parameters: undefined
+				}, token),
+				tool
+			});
+		}
+	}
+
+	if (toolCalls.length) {
+		const assistantMsg = vscode.LanguageModelChatMessage.Assistant('');
+		assistantMsg.content2 = toolCalls.map(toolCall => new vscode.LanguageModelChatResponseToolCallPart(toolCall.tool.id, toolCall.call.toolCallId, toolCall.call.parameters));
+		messages.push(assistantMsg);
+		for (const toolCall of toolCalls) {
+			// NOTE that the result of calling a function is a special content type of a USER-message
+			const message = vscode.LanguageModelChatMessage.User('');
+
+			message.content2 = [new vscode.LanguageModelChatMessageToolResultPart(toolCall.call.toolCallId, (await toolCall.result)['text/plain']!)];
+			messages.push(message);
+		}
+
+		// IMPORTANT The prompt must end with a USER message (with no tool call)
+		messages.push(vscode.LanguageModelChatMessage.User(`Above is the result of calling the functions ${toolCalls.map(call => call.tool.id).join(', ')}. The user cannot see this result, so you should explain it to the user if referencing it in your answer.`));
+
+		return invokeModelWithTools(messages, model, tools, request, response, token);
+	}
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
