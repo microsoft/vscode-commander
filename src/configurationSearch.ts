d44a5f4236c89a5a74a38e74fd46eaaf1c426f4e
BeniBenj <besimmonds@microsoft.com>
2024-10-08 10:34:59 +0200

    support $refs

--- src/configurationSearch.ts
+++ src/configurationSearch.ts
@@ -1,8 +1,7 @@
 import * as vscode from 'vscode';
 import MiniSearch from 'minisearch';
-import { assert } from 'console';
 
-type Property = { id: string, defaultValue: any; type: string; description?: string; markdownDescription?: string; enum?: string[]; enumDescriptions?: string[]; markdownEnumDescription?: string[] };
+type Property = { $ref?: string, id: string, defaultValue: any; type: string; description?: string; markdownDescription?: string; enum?: string[]; enumDescriptions?: string[]; markdownEnumDescription?: string[] };
 
 export type Configuration = { key: string, default: any; type: string; description: string; };
 
@@ -11,15 +10,31 @@ const configurationSearch = Promise.resolve(setupSearch());
 async function setupSearch(): Promise<MiniSearch<Configuration>> {
 	const defaultSettingsSchemaResource = vscode.Uri.parse('vscode://schemas/settings/default');
 	const textDocument = await vscode.workspace.openTextDocument(defaultSettingsSchemaResource);
-	const settings = JSON.parse(textDocument.getText()) as { properties: { [key: string]: Property } };
+	const settings = JSON.parse(textDocument.getText()) as { properties: { [key: string]: Property }, '$defs': { [key: string]: Property } };
 
 	const properties = Object.keys(settings.properties).
 		filter(key => !key.startsWith('[')).
 		map((key, id) => {
-			const property = settings.properties[key];
+			let property = settings.properties[key];
+
+			const reference = property['$ref'];
+			if (reference !== undefined && reference.startsWith('#/$defs/')) {
+				const referenceNumber = reference.split('/').pop();
+				if (referenceNumber === undefined) {
+					throw new Error('Reference number is undefined');
+				}
+
+				const referencedProperty = settings['$defs'][referenceNumber];
+				if (referencedProperty === undefined) {
+					throw new Error('Referenced property does not exist');
+				}
+
+				property = referencedProperty;
+			}
+
 			let description = property.markdownDescription ?? property.description ?? '';
 			if (property.type === 'string' && property.enum && (property.enumDescriptions || property.markdownEnumDescription)) {
-				description += '\nEnums:\n' + enumsDescription(property.enum, property.enumDescriptions ?? property.markdownEnumDescription ?? []);
+				description += '\nAllowed Values:\n' + enumsDescription(property.enum, property.enumDescriptions ?? property.markdownEnumDescription ?? []);
 			}
 
 			return {
@@ -41,17 +56,14 @@ async function setupSearch(): Promise<MiniSearch<Configuration>> {
 }
 
 async function searchConfiguration(keywords: string): Promise<Configuration[]> {
-	const search = (await configurationSearch).search;
-
 	// search for exact match on key
-	let results = search(keywords, { fields: ['key'], prefix: true, filter: (result => result.key === keywords) });
+	let results = (await configurationSearch).search(keywords, { fields: ['key'], prefix: true, filter: (result => result.key === keywords) });
 	if (results.length === 0) {
 		// search based on configuration id and description
-		results = search(keywords, { fields: ['key', 'description'] });
+		results = (await configurationSearch).search(keywords, { fields: ['key', 'description'] });
 	}
 
 	return results.
-		filter(result => result.score > 10).
 		map(result => ({
 			key: result.key,
 			default: result.default,
@@ -66,9 +78,8 @@ export async function getConfigurationsFromKeywords(keywords: string, limit: num
 }
 
 function enumsDescription(enumKeys: string[], enumDescriptions: string[]): string {
-	assert(enumKeys.length === enumDescriptions.length);
-	return enumKeys.map((key, index) => {
-		const description = enumDescriptions[index];
-		return `${key}: ${description}`;
+	return enumKeys.map((enumKey, index) => {
+		const enumDescription = enumDescriptions[index];
+		return `${enumKey}: ${enumDescription}`;
 	}).join('\n');
 }
