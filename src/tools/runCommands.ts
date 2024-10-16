540373f005117c96f85587791a2c0bdd6b970653
BeniBenj <besimmonds@microsoft.com>
2024-10-16 18:30:20 +0200

    Refactor argument parsing and command execution in RunCommands class

--- src/tools/runCommands.ts
+++ src/tools/runCommands.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
-import { Command, Configurations } from '../configurationSearch';
+import { Configurations, Command } from '../configurationSearch';
 
 const confirmationSettings: { [key: string]: vscode.LanguageModelToolConfirmationMessages } = {
    'workbench.action.resetViewLocations': {
@@ -24,8 +24,7 @@ export class RunCommands implements vscode.LanguageModelTool<{ key?: string, arg
       private readonly ranCommands: { key: string, arguments: any }[],
       private readonly configurations: Configurations,
       private readonly logger: vscode.LogOutputChannel,
-   ) {
-   }
+   ) { }
 
    prepareToolInvocation(options: vscode.LanguageModelToolInvocationPrepareOptions<{ key?: string, argumentsArray?: string }>, token: vscode.CancellationToken): vscode.ProviderResult<vscode.PreparedToolInvocation> {
       // validate parameters
@@ -45,45 +44,71 @@ export class RunCommands implements vscode.LanguageModelTool<{ key?: string, arg
          return { 'text/plain': 'Not able to change because the parameter is missing or invalid' };
       }
 
+      // Make sure the command exists
       const commands = await this.configurations.search(options.parameters.key, 1) as Command[];
       if (commands.length === 0) {
          return { 'text/plain': `Command ${options.parameters.key} not found` };
       }
       const [command] = commands;
 
-      let args: any[] = [];
-      if (options.parameters.argumentsArray) {
-         // parse arguments
-         try {
-            args = JSON.parse(options.parameters.argumentsArray);
-         } catch (e) {
-            this.logger.warn('Failed to parse args as JSON', e);
-         }
-
-         // If arguments are complex, we need to make sure they are valid
-         if (commandsWithComplexArguments.has(command.key)) {
-            const response = await this.validateComplexArguments(command, args, token);
-            if (typeof response === 'string') {
-               return { 'text/plain': response };
-            }
-         }
+      // Parse arguments
+      const parsedArgs = await this.parseArguments(options.parameters.argumentsArray, command, token);
+      if (parsedArgs.errorMessage) {
+         return { 'text/plain': 'Cancelled' };
       }
+      const args = parsedArgs.args ?? [];
 
-      this.logger.info(`Running ${options.parameters.key}` + (args ? ` with args ${JSON.stringify(args)}` : ''));
+      this.logger.info(`Running ${command.key}` + (args.length ? ` with args ${JSON.stringify(args)}` : ''));
 
+      // Run the command
       let response: unknown = undefined;
       try {
-         response = await vscode.commands.executeCommand(options.parameters.key, ...args);
-         this.ranCommands.push({ key: options.parameters.key, arguments: options.parameters.argumentsArray });
+         // Some commands require the editor to be focused to work correctly
+         if (this.requiresEditorFocus(command.key)) {
+            await vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup');
+         }
+
+         response = await vscode.commands.executeCommand(command.key, ...args);
+         this.ranCommands.push({ key: command.key, arguments: args });
       } catch (e: any) {
-         return { 'text/plain': `Wasn't able to run ${options.parameters.key} because of ${e.message}` };
+         return { 'text/plain': `Wasn't able to run ${command.key} because of ${e.message}` };
       }
 
+      // Return the result
       let responseMessage = response === undefined
-         ? `Command ${options.parameters.key} has been executed`
-         : `The result of executing the command ${options.parameters.key} is ${JSON.stringify(response)}`;
+         ? `Command ${command.key} has been executed`
+         : `The result of executing the command ${command.key} is ${JSON.stringify(response)}`;
 
       return { 'text/plain': responseMessage };
+   }
+
+   private async parseArguments(argsArray: string | undefined, command: Command, token: vscode.CancellationToken): Promise<{ errorMessage?: string, args?: any[] }> {
+      if (!argsArray) {
+         return { args: [] };
+      }
+
+      let args: any[] = [];
+      try {
+         args = JSON.parse(argsArray);
+      } catch (e) {
+         this.logger.warn('Failed to parse args as JSON', e);
+      }
+
+      // If arguments are complex, we need to make sure they are valid
+      if (commandsWithComplexArguments.has(command.key)) {
+         const response = await this.validateComplexArguments(command, args, token);
+         if (typeof response === 'string') {
+            return { 'errorMessage': response };
+         }
+      }
+
+      return { args };
+   }
+
+   private requiresEditorFocus(commandId: string): boolean {
+      return commandId.startsWith('editor.') ||
+         (commandId.startsWith('cursor') && !commandId.includes('.')) ||
+         (commandId.startsWith('editor') && !commandId.includes('.'));
    }
 
    private _lastComplexPrompt: string | undefined = undefined;
